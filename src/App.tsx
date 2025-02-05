24be094edb500401f2e752317ea0d0768e9a8751
Camila Paladines <camilapaladines27@gmail.com>
2025-02-05 13:45:04 -0500

    Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import {useState} from 'react';
+import './styles/index.css';
 
 // Assets
 import ArrowIcon from './assets/arrow-icon.svg';
 import LocationIcon from './assets/location-icon.svg';
+import BGDesktop from './assets/bg-desktop.png';
+import BGMobile from './assets/bg-mobile.png';
 
 function App() {
 	const [ipAddress, setIpAddress] = useState<string>('');
@@ -17,44 +20,67 @@ function App() {
 	const onSearch = () => {};
 
 	return (
-		<>
-			<header>
-				<h1>IP Address Tracker</h1>
-			</header>
-			<main>
-				<form>
+		<div className='flex flex-col items-center'>
+			<main className='flex flex-col items-center w-full'>
+				<h1 className='text-white font-bold text-3xl mt-[30px]'>IP Address Tracker</h1>
+				<form className='w-[555px] flex flex-row mt-7 mb-12'>
 					<input
 						value={ipAddress}
 						onChange={(e) => setIpAddress(e.target.value)}
 						placeholder='Search for any IP address or domain'
+						className='text-lg rounded-l-2xl bg-white py-[15px] px-6 flex-1 text-black outline-none'
 					/>
-					<button type='button' onClick={onSearch}>
+					<button
+						type='button'
+						onClick={onSearch}
+						className='bg-black rounded-r-2xl py-5 px-6 cursor-pointer hover:bg-[var(--dark-gray)]'>
 						<img src={ArrowIcon} />
 					</button>
 				</form>
-				<section>
-					<ul>
-						<li>
-							<p>IP Address</p>
-							<p>{info.ip}</p>
+				<section className='relative w-full'>
+					<img src={BGDesktop} className='absolute bottom-[50%] w-full -z-10' />
+					<ul className='flex flex-row w-[1110px] py-[35px] px-8 rounded-2xl bg-white mx-auto'>
+						<li className='flex flex-col gap-3 flex-1'>
+							<p className='uppercase color-light-gray text-xs tracking-[2px] font-semibold'>
+								IP Address
+							</p>
+							<p className='color-dark-gray text-2xl font-semibold tracking-[0.6px]'>
+								{info.ip}
+							</p>
 						</li>
-						<li>
-							<p>Location</p>
-							<p>{info.location}</p>
+						<div className='h-[75px] w-[1px] bg-light-gray my-auto mx-8' />
+						<li className='flex flex-col gap-3 flex-1'>
+							<p className='uppercase color-light-gray text-xs tracking-[2px] font-semibold'>
+								Location
+							</p>
+							<p className='color-dark-gray text-2xl font-semibold tracking-[0.6px]'>
+								{info.location}
+							</p>
 						</li>
-						<li>
-							<p>Timezone</p>
-							<p>{info.timezone}</p>
+						<div className='h-[75px] w-[1px] bg-light-gray my-auto mx-8' />
+						<li className='flex flex-col gap-3 flex-1'>
+							<p className='uppercase color-light-gray text-xs tracking-[2px] font-semibold'>
+								Timezone
+							</p>
+							<p className='color-dark-gray text-2xl font-semibold tracking-[0.6px]'>
+								{info.timezone}
+							</p>
 						</li>
-						<li>
-							<p>ISP</p>
-							<p>{info.isp}</p>
+						<div className='h-[75px] w-[1px] bg-light-gray my-auto mx-8' />
+
+						<li className='flex flex-col gap-3 flex-1'>
+							<p className='uppercase color-light-gray text-xs tracking-[2px] font-semibold'>
+								ISP
+							</p>
+							<p className='color-dark-gray text-2xl font-semibold tracking-[0.6px]'>
+								{info.isp}
+							</p>
 						</li>
 					</ul>
 				</section>
 			</main>
 			<footer></footer>
-		</>
+		</div>
 	);
 }
 
